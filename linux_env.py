2ffd054cdb76241addab9a5140f8b5fe3f6813a6
yaotian <yaotian@yaotian-HP-Compaq-6910p-GU311UC-AB2.(none)>
2012-04-27 22:12:31 +0800

    init
    Signed-off-by:  <>

---
+++ linux_env.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+class BashFile(object):
+	"""docstring for BashFile
+	This class is to operate the BashFile
+	"""
+	def __init__(self, arg):
+		super(BashFile, self).__init__()
+		self.arg = arg
+
+
+	def add_property(self,key,value):
+		pass
+
+
